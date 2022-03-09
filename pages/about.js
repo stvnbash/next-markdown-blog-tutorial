074f8970f1ed004bcaf5cd39debd6781320aa8a5
stvnbash <56286042+stvnbash@users.noreply.github.com>
2022-03-09 14:27:55 -0800

    Fixed issue with quote charater

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ export default function about() {
                 <h1 className='post-title'>About</h1>
                 <div className='post-body'>
                     <div>
-                        <p>This app was created by Steven Bash following the "Static Blog With Next.js and Markdown" tutorial by Traversy Media</p>
+                        <p>This app was created by Steven Bash following the &quot;Static Blog With Next.js and Markdown&quot; tutorial by Traversy Media</p>
                     </div>
                 </div>
             </div>
