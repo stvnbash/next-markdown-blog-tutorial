faaf7dc48353fdec75be8227cca55c0d54b4b605
stvnbash <56286042+stvnbash@users.noreply.github.com>
2022-03-09 13:05:20 -0800

    built for production

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ posts }) {
 
       <div className='posts'>
         {posts.map((post, index) => (
-          <Post post={post} />
+          <Post key={index} post={post} />
         ))}
       </div>
     </div>
